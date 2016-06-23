cf16bd8a6c4736f70b70ad600a039fafc31f51eb
JennyAyna <phuongnghi@hotmail.fr>
2016-06-24 01:22:34 +0200

    init portfolio teaser

---
+++ README.md
@@ -0,0 +1,11 @@
+---------
+
+Boilerplate for Vue.js project with Grunt tasks, Sass & Greensock
+
+---------
+
+
+TO DO BEFORE TRUE DEV :
+
+- [ ] Make sure to have Node.js, Sass, Git and Grunt installed in your machine.
+- [ ] Remove unnecessary Greensock libs.
---
+++ src/js/scripts.js
@@ -0,0 +1,19 @@
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+    //console.log('Hello');
+
+
+    new Vue({
+      el: '#app',
+      data: {
+        message: 'Coucou'
+      }
+    });
+
+
+  });
+
+})(jQuery, window, document);
